5b97078ab4743cf28c6bcbdb78a8c8300d91d093
ivanishah <72005922+ivanishah@users.noreply.github.com>
2023-06-07 22:37:01 -0400

    FIRST COMMIT

---
+++ FreeCRMTest/src/test/java/tests/FreeCRMTest/AppTest.java
@@ -0,0 +1,21 @@
+package tests.FreeCRMTest;
+
+import static org.junit.Assert.assertTrue;
+import static org.testng.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        //assertTrue( true );
+    }
+}
